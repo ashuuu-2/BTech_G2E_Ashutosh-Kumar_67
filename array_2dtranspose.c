1d7f12ec0a6704f975480d62ca213a196a2db5bb
Ashutosh Kumar <ashutosh0885333@gmail.com>
2026-04-12 17:06:41 +0530

    2D ARRAY TRANSPOSE
    WAP TO TRANSPOSE A 2D ARRAY OR 3X3 MATRIX

---
+++ array_2dtranspose.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a[3][3],b[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            b[j][i]=a[i][j];
+        }
+    }
+    for(int i=0;i<=2;i++){
+        for(int j=0;j<=2;j++){
+            printf("%d \t",b[i][j]);}}
+    return 0;
+}
